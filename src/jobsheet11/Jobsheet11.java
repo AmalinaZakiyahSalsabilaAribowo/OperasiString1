bed11cf0cb71ef69688adf44527f1f578e0aebb1
ASUS <ASUS@LAPTOP-929I2GH2>
2018-08-15 17:29:41 +0700

    Initial Commit

---
+++ src/jobsheet11/Jobsheet11.java
@@ -0,0 +1,12 @@
+package jobsheet11;
+import java.util.Locale;
+public class Jobsheet11 {
+
+    public static void main(String[] args) {
+        String identitas = "Amalina Zakiyah Salsabila Aribowo / XRPL2 / 04";
+        System.out.println("Identitas : "+ identitas);
+    
+    
+        
+    }
+}
