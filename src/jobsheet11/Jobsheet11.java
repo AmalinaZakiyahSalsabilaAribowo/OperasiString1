7e761cd93a2d1e47e363fcdcac9ddbb024b0a48a
ASUS <ASUS@LAPTOP-929I2GH2>
2018-08-16 14:47:56 +0700

    Coba method length & isEmpty.

--- src/jobsheet11/Jobsheet11.java
+++ src/jobsheet11/Jobsheet11.java
@@ -7,5 +7,15 @@ public class Jobsheet11 {
     public static void main(String[] args) {
         String identitas = "Amalina Zakiyah Salsabila Aribowo / XRPL2 / 04";
         System.out.println("Identitas : "+ identitas);
+        
+        String x = "Operasi";
+    System.out.println("isi variabel x : "+ x);
+    System.out.println("\""+ x +"\" panjang karakter = "+ x.length());
+    
+    System.out.println("x adalah ksosong : " + x.isEmpty());
+    
+    String y= "";
+    System.out.println("isi variabel y : "+ y);
+    System.out.println("y adalah kosong : " + y.isEmpty());
 }
 }
