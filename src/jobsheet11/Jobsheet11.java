237281a07acc9a5062665b9f43ec4e7dd62f8c4a
ASUS <ASUS@LAPTOP-929I2GH2>
2018-08-16 14:57:05 +0700

    Coba method compareTo & compareToIgnoreCase.

--- src/jobsheet11/Jobsheet11.java
+++ src/jobsheet11/Jobsheet11.java
@@ -8,6 +8,8 @@ public class Jobsheet11 {
         String identitas = "Amalina Zakiyah Salsabila Aribowo / XRPL2 / 04";
         System.out.println("Identitas : "+ identitas);
         
+        
+        
         String x = "Operasi";
     System.out.println("isi variabel x : "+ x);
     System.out.println("\""+ x +"\" panjang karakter = "+ x.length());
@@ -17,5 +19,33 @@ public class Jobsheet11 {
     String y= "";
     System.out.println("isi variabel y : "+ y);
     System.out.println("y adalah kosong : " + y.isEmpty());
+    
+    
+    
+    
+    System.out.println("isi x sama dengan y : " + x.equals(y));
+   
+   String z = "Operasi";
+   System.out.println("isi variabel z : "+ z);
+   System.out.println("isi x sama dengan z (Case Sensistive) : " + x.equals(z));
+   
+   String r = "operasi";
+   System.out.println("isi variabel r : "+ z);
+   System.out.println("isi x sama dengan r (Case Sensitive) : " + x.equals(r));
+   System.out.println("isi x sama dengan r (Not Case Sensitive) : " + x.equalsIgnoreCase(r));
+   
+   
+   
+   System.out.println("perbandingan isi x dengan y : " + x.compareTo(y));
+   System.out.println("perbandingan isi x dengan z (Case Sensitive): " +x.compareTo(z));
+   System.out.println("perbandingan isi x dengan r (Case Sensitive): " +x.compareTo(r));
+   System.out.println("perbandingan isi x dengan r (Not Case Sensitive): " + x.compareToIgnoreCase(r));
+   
+   String s = "operasI";
+   System.out.println("isi variabel s : "+ s);
+   System.out.println("perbandingan isi r dengan s (Case Sensitive) : " + r.compareTo(s));
+   
+   
+    
 }
 }
