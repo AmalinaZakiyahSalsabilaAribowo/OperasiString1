09f49a605f96bece219a0a707b7392b377d4d25a
ASUS <ASUS@LAPTOP-929I2GH2>
2018-08-16 15:09:07 +0700

    Coba method substring, replace, & split.

--- src/jobsheet11/Jobsheet11.java
+++ src/jobsheet11/Jobsheet11.java
@@ -63,7 +63,23 @@ public class Jobsheet11 {
    
    
    
+    System.out.println("karakter mulai posisi 3 di variabel x : " + x.substring(3));
    
+   System.out.println("karakter dari posisi 3-5  di variabel x : " + x.substring(3,6));
+   
+   String tolean = t.trim();
+   System.out.println("isi variabel tolean : " + t.trim());
+   System.out.println("rubah 'i' jadi 'E' di variabel tolean : " + tolean.replace("i", "E"));
+   
+   String[] arrT = tolean.split(" ");
+   System.out.println("pecah kata di variabel tolean : ");
+   for (int i=0;i<arrT.length;i++)
+       System.out.println(arrT[i]);
+   
+   String[] arrTr = tolean.split("r");
+   System.out.println("pecah berdasarkan huruf 'r' di variabel tolean : ");
+   for (int i=0;i<arrTr.length;i++)
+       System.out.println(arrTr[i]);
    
 }
 }
